6683c73a0af4902d0bc59f349a12c8cf3c9bac87
Jonathan Ko <jonathanko@Jonathans-MacBook-Pro.local>
2018-07-14 21:04:37 -0700

    first commit

---
+++ arrayDestructuring.js
@@ -0,0 +1,9 @@
+var location_one, location_two, location_three
+
+[location_one, location_two, location_three] = ['New York', 'Los Angeles', 'San Francisco']
+
+console.log(location_one)
+
+console.log(location_two)
+
+console.log(location_three)
